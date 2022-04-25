8664cfd7db30e2c6551d4cec363b1efe31b9ee45
miloNeat <jack_nolan@yeah.net>
2022-04-26 01:14:38 +0900

    build with params

--- src/Jenkinsfile/projectA-stages.groovy
+++ src/Jenkinsfile/projectA-stages.groovy
@@ -1,18 +1,32 @@
 import hudson.model.*;
-
-
-pipeline {
-
-    agent any
-    stages{
-        stage("Hello Pipeline") {
-            steps {
-                script {
-                    println "Hello Pipeline!"
-                    println env.JOB_NAME
-                    println env.BUILD_NUMBER
-                }
+ 
+ 
+pipeline{
+ 
+   agent any
+   stages{
+      stage("Hello Pipeline") {
+         steps {
+             script {
+                println "Hello Pipeline!"
+                println env.JOB_NAME
+                println env.BUILD_NUMBER
             }
-        }
-    }
+         }
+      }
+ 
+      stage("Init paramters in json") {
+         steps {
+             script {
+ 
+                println "read josn input file"
+                json_file = INPUT_JSON? INPUT_JSON.trim() : ""
+                prop = readJSON file : json_file
+                name = prop.NAME? prop.NAME.trim() : ""
+                println "Name:" + name
+            }
+         }
+      }
+   }
+ 
 }
